fdbc8635b39414806fc6f98df19ced8bd35a296c
Suman Kumar <sumancsnit@gmail.com>
2024-10-21 22:03:11 +0530

    update: code format

--- src/features/bookings/BookingTable.jsx
+++ src/features/bookings/BookingTable.jsx
@@ -1,22 +1,22 @@
-import BookingRow from "./BookingRow";
-import Table from "../../ui/Table";
-import Menus from "../../ui/Menus";
-import Empty from "../../ui/Empty";
+import BookingRow from './BookingRow';
+import Table from '../../ui/Table';
+import Menus from '../../ui/Menus';
+import Empty from '../../ui/Empty';
 
-import { useBookings } from "./useBookings";
-import Spinner from "../../ui/Spinner";
-import Pagination from "../../ui/Pagination";
+import { useBookings } from './useBookings';
+import Spinner from '../../ui/Spinner';
+import Pagination from '../../ui/Pagination';
 
-function BookingTable() {
+const BookingTable = () => {
   const { bookings, isLoading, count } = useBookings();
 
   if (isLoading) return <Spinner />;
 
-  if (!bookings.length) return <Empty resourceName="bookings" />;
+  if (!bookings.length) return <Empty resourceName='bookings' />;
 
   return (
     <Menus>
-      <Table columns="0.6fr 2fr 2.4fr 1.4fr 1fr 3.2rem">
+      <Table columns='0.6fr 2fr 2.4fr 1.4fr 1fr 3.2rem'>
         <Table.Header>
           <div>Cabin</div>
           <div>Guest</div>
@@ -39,6 +39,6 @@ function BookingTable() {
       </Table>
     </Menus>
   );
-}
+};
 
 export default BookingTable;
--- src/hooks/useOutsideClick.js
+++ src/hooks/useOutsideClick.js
@@ -1,23 +1,20 @@
-import { useEffect, useRef } from "react";
+import { useEffect, useRef } from 'react';
 
-export function useOutsideClick(handler, listenCapturing = true) {
+export const useOutsideClick = (handler, listenCapturing = true) => {
   const ref = useRef();
 
-  useEffect(
-    function () {
-      function handleClick(e) {
-        if (ref.current && !ref.current.contains(e.target)) {
-          handler();
-        }
+  useEffect(() => {
+    function handleClick(e) {
+      if (ref.current && !ref.current.contains(e.target)) {
+        handler();
       }
+    }
 
-      document.addEventListener("click", handleClick, listenCapturing);
+    document.addEventListener('click', handleClick, listenCapturing);
 
-      return () =>
-        document.removeEventListener("click", handleClick, listenCapturing);
-    },
-    [handler, listenCapturing]
-  );
+    return () =>
+      document.removeEventListener('click', handleClick, listenCapturing);
+  }, [handler, listenCapturing]);
 
   return ref;
-}
+};
